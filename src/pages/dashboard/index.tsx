6a46d7c9eaa6f8367794388072d79415d1f09c3c
Vinicius Cesar <viniciuscesar027@gmail.com>
2024-08-13 12:10:56 -0300

    dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,17 @@
 import { GetServerSideProps } from "next";
+import { ChangeEvent, useState } from "react";
 import styles from "./styles.module.css";
 import Head from "next/head";
 import Textarea from "../../components/textarea";
-import { FiShare2 } from 'react-icons/fi'
-import { FaTrash } from 'react-icons/fa'
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,12 +22,14 @@ export default function Dashboard() {
           <div className={styles.contentForm}>
             <h1 className={styles.title}>Qual sua tarefa?</h1>
             <form>
-                <Textarea placeholder="Digite qual sua tarefa"/>
-                <div className={styles.checkboxArea}>
-                    <input type="checkbox" className={styles.checkbox} />
-                    <label>Deixar tarefa pública?</label>
-                </div>
-                <button type="submit" className={styles.button}>Registrar</button>
+              <Textarea placeholder="Digite qual sua tarefa" value={input} onChange={ (e:ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value) }/>
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa pública?</label>
+              </div>
+              <button type="submit" className={styles.button}>
+                Registrar
+              </button>
             </form>
           </div>
         </section>
@@ -35,19 +41,13 @@ export default function Dashboard() {
             <div className={styles.tagContainer}>
               <label className={styles.tag}>PUBLICO</label>
               <button className={styles.shareButton}>
-                <FiShare2 
-                  size={22}
-                  color="#3183ff"
-                />
+                <FiShare2 size={22} color="#3183ff" />
               </button>
             </div>
             <div className={styles.taskContent}>
               <p>My first task</p>
               <button className={styles.trashButton}>
-                <FaTrash 
-                size={24}
-                color="#ea3140"
-                />
+                <FaTrash size={24} color="#ea3140" />
               </button>
             </div>
           </article>
